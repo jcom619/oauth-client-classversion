f622d65bd5eba9e60f061ff44fd0acdfba2c25c2
h64 <hhongnr@gmail.com>
2021-04-08 15:36:13 -0700

    handle token coming in from backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,8 +8,12 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import SaveToken from './pages/SaveToken'
 
 const App = () => {
+
+    const [user, setUser] = useState(null)
+
     return (
         <Router>
             <Navbar />
@@ -22,6 +27,9 @@ const App = () => {
                     </Route>
                     <Route path="/login">
                         <Login/>
+                    </Route>
+                    <Route path='/saveToken'>
+                        <SaveToken setUser={setUser} />
                     </Route>
                 </Switch>
             </div>
---
+++ src/pages/SaveToken.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react"
+import { Redirect, useLocation } from "react-router"
+import jwt from 'jsonwebtoken'
+
+const SaveToken = ({ setUser }) => {
+    const query = new URLSearchParams(useLocation().search)
+    const token = query.get('token')
+
+    // https://reactrouter.com/web/example/query-parameters
+    useEffect(() => {
+        // grabs the token from the query param and saves it to localstorage
+        console.log('token is:', token)
+        localStorage.setItem('jwt', token)
+
+        // We decode the user from the JWT
+        const user = jwt.decode(token)
+
+        // save that user to the app state 
+        setUser(user)
+    }, [setUser, token])
+
+    if(token) {
+        return <Redirect to="/" />
+    } else {
+        return <Redirect to="/login" />
+    }
+}
+
+export default SaveToken
