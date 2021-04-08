18ddb0fb38840944e3849c896ba827cc9163d2b7
h64 <hhongnr@gmail.com>
2021-04-08 11:46:16 -0700

    checkpoint #2 - cutesie buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,24 @@
+import {
+    GoogleLoginButton,
+    GithubLoginButton
+} from 'react-social-login-buttons'
+
 const Login = props => {
+
+    const handleGoogleClick = () => {
+        console.log('Google is clicked!')
+    }
+
+    const handleGithubClick = () => {
+        console.log('Github is clicked!')
+    }
+
     return (
-        <div>This is the Login page</div>
+        <div className="login">
+            <h1>Login</h1>
+            <GoogleLoginButton onClick={handleGoogleClick} />
+            <GithubLoginButton onClick={handleGithubClick} />
+        </div>
     )
 }
 
